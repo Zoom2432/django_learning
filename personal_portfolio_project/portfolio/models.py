778ea2632342084a10c858a756ad237417365596
Zoom2432 <vsgz2432@gmail.com>
2023-10-02 18:51:22 +0300

    Learning models

--- personal_portfolio_project/portfolio/models.py
+++ personal_portfolio_project/portfolio/models.py
@@ -3,5 +3,5 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=100)
     desc = models.CharField(max_length=250)
-    img = models.ImageField(upload_to='portfolio/images')
+    img = models.ImageField(upload_to='portfolio/images/')
     url = models.URLField(blank=True)
