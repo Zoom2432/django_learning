9b0c9530fcee2b65920be5b63ac9555ff2727568
Zoom2432 <vsgz2432@gmail.com>
2023-10-03 12:58:33 +0300

    Finished portfolio/blog web-site. Without styles.

--- personal_portfolio_project/personal_portfolio/settings.py
+++ personal_portfolio_project/personal_portfolio/settings.py
@@ -119,6 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
